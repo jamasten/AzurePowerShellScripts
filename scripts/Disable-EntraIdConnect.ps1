675a9b44a2988998df42e9b7023ad55997f436c0
Jason Masten (from Dev Box) <jamasten@microsoft.com>
2024-03-27 14:02:35 -0400

    Added files

---
+++ scripts/Disable-EntraIdConnect.ps1
@@ -0,0 +1,33 @@
+﻿# TIP: Use the Global Admin account used to setup AD Connect
+
+Param(
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('Global','China','Germany','USGov','USGovDoD')]
+    [string]$Environment,
+
+    [Parameter(Mandatory=$true)]
+    [string]$TenantId
+
+)
+
+$UriSuffix = switch($Environment)
+{
+    Global { 'com' }
+    USGov  { 'us'  }
+}
+
+if(!(Get-Module -ListAvailable | Where-Object {$_.Name -eq 'Microsoft.Graph.Identity.DirectoryManagement'}))
+{
+    Install-Module -Name 'Microsoft.Graph.Identity.DirectoryManagement' -AllowClobber -Force
+}
+
+if(!(Get-MgOrganization -ErrorAction SilentlyContinue))
+{
+    Connect-MgGraph -Environment $Environment -TenantId $TenantId -Scopes 'Organization.ReadWrite.All'
+}
+
+$OrgId = (Get-MgOrganization).id
+$Uri = 'https://graph.microsoft.' + $UriSuffix + '/beta/organization/' + $OrgId
+$Body = @{onPremisesSyncEnabled = 'false'} | ConvertTo-Json
+Invoke-MgGraphRequest -Uri $Uri -Body $Body -Method 'PATCH'
---
+++ scripts/Get-AzureImageVersion.ps1
@@ -0,0 +1,22 @@
+﻿$location = 'eastus'
+#$location = 'usgovvirginia'
+
+$publisher = 'MicrosoftWindowsDesktop'
+#$publisher = 'MicrosoftWindowsServer'
+
+$offer = 'office-365'
+#$offer = 'Windows-11'
+#$offer = 'Windows-10'
+#$offer = 'WindowsServer'
+
+$sku = 'win11-21h2-avd-m365'
+#$sku = '21h1-evd-o365pp'
+#$sku = '2019-Datacenter-Core'
+
+(Get-AzVMImagePublisher -Location $location).PublisherName
+
+(Get-AzVMImageOffer -Location $location -PublisherName $publisher).Offer
+
+(Get-AzVMImageSku -Location $location -PublisherName $publisher -Offer $offer).Skus
+
+Get-AzVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku | Select-Object * | Format-List
